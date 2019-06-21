e390ec20b55de8b7fc79bc09c8a101f6994f3c36
okada001 <okada001@rangers.uwp.edu>
2019-06-21 16:23:40 +0900

    fixed some code

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ function animatedForm(){
 function validateUser (user){
     if(user.value.length < 6){
         console.log("not enough characters");
-        error("rgb(189,87,87)");
+        changeBG("rgb(189,87,87)");
     }else{
-        error("rgb(87,189,130)");
+        changeBG("rgb(87,189,130)");
         return true;
     }
 }
@@ -39,10 +39,10 @@ function validateUser (user){
 function validateEmail (email){
     const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
     if (validation.test(email.value)){
-        error("rgb(87,189,130)");
+        changeBG("rgb(87,189,130)");
         return true;
     }else{
-        error("rgb(189,87,87)");
+        changeBG("rgb(189,87,87)");
 
     }
 }
@@ -53,7 +53,7 @@ function nextSlide (parent, nextForm){
     nextForm.classList.add("active");
 }
 
-function error(color){
+function changeBG(color){
     document.body.style.backgroundColor = color;
 }
 
